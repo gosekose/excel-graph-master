37a13e6913e4d7be10685717cb09dab75e4146ca
gosekose <ksyn1611@naver.com>
2023-04-23 02:17:49 +0900

    Feat: 프로젝트 생성

---
+++ src/main/java/excel/excelgraphmaster/ExcelGraphMasterApplication.java
@@ -0,0 +1,13 @@
+package excel.excelgraphmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelGraphMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelGraphMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/excel/excelgraphmaster/ExcelGraphMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package excel.excelgraphmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelGraphMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
